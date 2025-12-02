0eeee750b3643f64be4c3a4a93b82a10bca9c8c0
unknown <gabriel31alves@gmail.com>
2025-12-02 10:50:45 -0300

    Mudanças em pedidos

--- src/controllers/pedidoController.js
+++ src/controllers/pedidoController.js
@@ -1,4 +1,6 @@
 const { pedidoModel } = require('../models/pedidoModel');
+const { clienteModel } = require('../models/clienteModel');
+const geolib = require('geolib');
 
 const pedidoController = {
     consultarPedidoPorId: async (req, res) => {
@@ -8,11 +10,12 @@ const pedidoController = {
             if (!idPedido || typeof idPedido != 'number') {
                 return res.status(200).json({ message: 'Verificar os dados enviados e tente novamete' });
             }
+
+            const resultado = await pedidoModel.selectPedidoById(idPedido);
+
             if (resultado.length === 0) {
                 return res.status(200).json({ message: 'A consulta não retornou resultados' });
             }
-
-            const resultado = await pedidoModel.selectPedidoById(idPedido);
             return res.status(200).json({ data: resultado });
 
         } catch (error) {
@@ -32,30 +35,67 @@ const pedidoController = {
             res.status(500).json({ message: 'Ocorreu um erro no servidor', errorMessage: error.errorMessage });
         }
     },
+
     criarPedido: async (req, res) => {
         try {
             const idCliente = Number(req.params.idCliente);
             const idTipoEntrega = Number(req.params.idTipoEntrega);
 
-            const { distancia, pesoCarga, valorKM, valorKG } = req.body;
+            const { pesoCarga, valorKM, valorKG } = req.body;
+            // const dadosCliente = await clienteModel.selectById(idCliente);
 
-            const cepDistance = require("cep-distance");
+            const dadosCep = await (await fetch(`https://cep.awesomeapi.com.br/json/13199174`)).json();
+            if (dadosCep.code == 'ivalid') {
+                return res.status(400).json({ message: dadosCep.message });
+            }
 
-            const cepCliente = "01001000";
-            const cep2 = "01310200";
+            const saidaGalpao = { latitude: -22.81937, longitude: -47.27421 };
+            const casaCliente = { latitude: dadosCep.lat, longitude: dadosCep.lng };
 
-                const distanceInKilometers = await cepDistance(cepCliente, cep2, "KM");
-                console.log(`${distanceInKilometers} KiloMetros`); // 2.66 KiloMetros
-        
+            const distanciaMetros = geolib.getDistance(saidaGalpao, casaCliente);
+            const distanciaKm = (distanciaMetros / 1000).toFixed(2);
 
-            if (!idCliente || !idTipoEntrega || !distancia || !pesoCarga || !valorKM || !valorKG || typeof idCliente != 'number' || typeof idTipoEntrega != 'number') {
+
+            if (!idCliente || !idTipoEntrega || !distanciaKm || !pesoCarga || !valorKM || !valorKG || typeof idCliente != 'number' || typeof idTipoEntrega != 'number') {
                 return res.status(400).json({ message: 'Verificar os dados enviados e tente novamete' });
             }
 
-            const resultado = await pedidoModel.insertPedido(distanceInKilometers, pesoCarga, valorKM, valorKG, idCliente, idTipoEntrega);
+            const resultado = await pedidoModel.insertPedido(distanciaKm, pesoCarga, valorKM, valorKG, idCliente, idTipoEntrega);
 
             res.status(201).json({ message: 'Registro incluído com sucesso.', data: resultado });
 
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Ocoreu um erro no servidor', errorMessage: error.message });
+        }
+    },
+    alterarPedido: async (req, res) => {
+        try {
+            const idPedido = Number(req.params.idPedido);
+            const { idTipoEntrega, distanciaKm, pesoCarga, valorKM, valorKG } = req.body;
+            console.log(distanciaKm);
+
+            // if (!idPedido || !idTipoEntrega || !distanciaKm || !pesoCarga || !valorKM || !valorKG || typeof idTipoEntrega != 'number' || typeof idPedido != 'number') {
+            //     return res.status(400).json({ message: 'Verificar os dados enviados e tente novamete' });
+            // }
+            console.log(idPedido)
+            const pedidoAtual = await pedidoModel.selectById(idPedido);
+            if (pedidoAtual.length === 0) {
+                return res.status(200).json({ message: 'Pedido não localizado no sistema' });
+            }
+            console.log(pedidoAtual)
+
+            const novaDistancia = distanciaKm ?? pedidoAtual[0].distancia;
+            console.log(distanciaKm);
+
+            const novoPeso = pesoCarga ?? pedidoAtual[0].peso_carga;
+            const novoValorKm = valorKM ?? pedidoAtual[0].valor_km;
+            const novoValorKg = valorKG ?? pedidoAtual[0].valor_kg;
+            const novoTipoEntrega = idTipoEntrega ?? pedidoAtual[0].id_tipo_entrega;
+
+            console.log(novoTipoEntrega)
+            const resultado = await pedidoModel.updatePedido(idPedido, novaDistancia, novoPeso, novoValorKm, novoValorKg, novoTipoEntrega);
+            res.status(201).json({ message: 'Registro incluído com sucesso.', data: resultado });
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Ocoreu um erro no servidor', errorMessage: error.message });
