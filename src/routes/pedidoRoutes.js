aca4d6d87e0e2d344868154a27dacdebca3dc7f1
unknown <gabriel31alves@gmail.com>
2025-12-08 09:53:09 -0300

    Atualização rota peido

--- src/routes/pedidoRoutes.js
+++ src/routes/pedidoRoutes.js
@@ -5,7 +5,7 @@ const { pedidoController } = require('../controllers/pedidoController');
 pedidoRoutes.get('/pedidos/:idPedido', pedidoController.consultarPedidoPorId);
 pedidoRoutes.get('/pedidos', pedidoController.consultaPedidos);
 pedidoRoutes.post('/pedidos/:idCliente/:idEndereco/:idTipoEntrega', pedidoController.criarPedido);
-pedidoRoutes.patch('/pedidos/:idPedido/endereco/:idEndereco', pedidoController.alterarPedido);
+pedidoRoutes.put('/pedidos/:idPedido/endereco/:idEndereco', pedidoController.alterarPedido);
 pedidoRoutes.patch('/pedidos/:idPedido/status/:idStatusEntrega', pedidoController.alterarStatusPedido);
 pedidoRoutes.delete('/pedidos/delete/:idPedido', pedidoController.deletarPedido);
 
