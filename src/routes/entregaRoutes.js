e1282d8710b1850bc43e5f1480389d598fd45ed5
unknown <gabriel31alves@gmail.com>
2025-12-08 09:35:59 -0300

    atualizaçao routes

--- src/routes/entregaRoutes.js
+++ src/routes/entregaRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const entregaRoutes = express.Router();
 const { entregaController } = require('../controllers/entregaController');
 
-entregaRoutes.post('/entregas/:idPedido/:idFuncionario', entregaController.criarEntrega);
 entregaRoutes.get('/entregas', entregaController.consultarEntregas);
+entregaRoutes.get('/entregas/pedido/:idPedido/', entregaController.consultarEntregaPorPedido);
 entregaRoutes.get('/entregas/:idEntrega', entregaController.consultarEntregaPorId);
+entregaRoutes.post('/entregas', entregaController.criarEntrega);
 entregaRoutes.patch('/entregas/:idEntrega', entregaController.alterarEntrega);
 entregaRoutes.delete('/entregas/delete/:idEntrega', entregaController.deletarEntrega);
 
